5a6f1a1a83790b9245bd93850b8d8c573c421c5d
Rohit Kumar <rohitmaurya1635@gmail.com>
2026-01-02 23:28:56 +0530

    fixed featuredProduct component

--- src/components/application/Website/FeaturedProducts.jsx
+++ src/components/application/Website/FeaturedProducts.jsx
@@ -1,28 +1,53 @@
+'use client'
+
+import React, { useEffect, useState } from 'react'
+
 import { IoIosArrowRoundForward } from 'react-icons/io'
 import Link from 'next/link'
 import ProductCard from './ProductCard'
-import React from 'react'
 import axios from 'axios'
 
-const FeaturedProducts = async () => {
+const FeaturedProducts = () => {
+    const [productData, setProductData] = useState(null)
 
-    const { data: productData } = await axios.get(`${process.env.NEXT_PUBLIC_API_BASE_URL}/product/get-featured-products`);
-    if (!productData) return null;
+    useEffect(() => {
+        const fetchProducts = async () => {
+            try {
+                const { data } = await axios.get(
+                    `${process.env.NEXT_PUBLIC_API_BASE_URL}/product/get-featured-products`
+                )
+                setProductData(data)
+            } catch (error) {
+                console.error(error)
+            }
+        }
+
+        fetchProducts()
+    }, [])
 
     return (
         <div>
             <div className='flex justify-between items-center mb-5'>
-                <h2 className='sm:text-4xl text-2xl font-semibold'>Featured Products</h2>
-                <Link href={"#"} className='flex items-center gap-2 underline underline-offset-4 hover:text-primary'>
-                    View All  <IoIosArrowRoundForward />
+                <h2 className='sm:text-4xl text-2xl font-semibold'>
+                    Featured Products
+                </h2>
+
+                <Link href="#" className='flex items-center gap-2 underline underline-offset-4 hover:text-primary'>
+                    View All <IoIosArrowRoundForward />
                 </Link>
             </div>
-            <div className='grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-cols-1 sm:gap-10 gap-2 '>
-                {!productData.success && <div className='text-center py-5'>Data not found</div>}
 
-                {productData.success && productData.data.map((product) => (
-                    <ProductCard key={product._id} product={product} />
-                ))}
+            <div className='grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-cols-1 sm:gap-10 gap-2'>
+                {!productData?.success && (
+                    <div className='text-center py-5 col-span-full'>
+                        Data not found
+                    </div>
+                )}
+
+                {productData?.success &&
+                    productData.data.map((product) => (
+                        <ProductCard key={product._id} product={product} />
+                    ))}
             </div>
         </div>
     )
